e1c145f0f784187a204363c4bee4c2145d591930
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-06-04 17:19:02 +0000

    Fixed setup.py file

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup
 
 setup(
     name="gym_continual_rl",
-    version="0.0.1",
-    install_requires=["gymnasium==0.29.1", "pygame==2.5.2"],
+    version="0.0.2",
+    install_requires=[
+        "gymnasium==0.29.1",
+        "numpy==1.26.4",
+        "pygame==2.5.2",
+    ],
+    packages=["gym_continual_rl"],
+    python_requires=">=3.9",
 )
