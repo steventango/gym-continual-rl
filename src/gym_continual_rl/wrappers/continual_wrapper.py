7433a447d0aa34dc4e0572d1373dfc49b26bc23c
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-06-11 22:26:51 +0000

    Move n_tasks to continual wrapper

--- src/gym_continual_rl/wrappers/continual_wrapper.py
+++ src/gym_continual_rl/wrappers/continual_wrapper.py
@@ -5,18 +5,19 @@ from gymnasium.core import WrapperObsType
 
 
 class ContinualWrapper(gym.Wrapper):
-    def __init__(self, env, change_task_every_n_episodes: int = 50):
+    def __init__(self, env, change_task_every_n_episodes: int = 50, n_tasks: int = 2):
         super().__init__(env)
         self.change_task_every_n_episodes = change_task_every_n_episodes
         self.episode_counter = 0
         self.task = 0
+        self.n_tasks = n_tasks
 
     def step(self, action):
         obs, reward, terminated, truncated, info = self.env.step(action)
-        if terminated:
+        if terminated or truncated:
             self.episode_counter += 1
             if self.episode_counter % self.change_task_every_n_episodes == 0:
-                self.task = (self.task + 1) % info["n_tasks"]
+                self.task = (self.task + 1) % self.n_tasks
         return obs, reward, terminated, truncated, info
 
     def reset(
