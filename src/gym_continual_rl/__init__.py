811aaa8a7e6d745739f0227644f1e733e46b2ba4
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-08-12 20:13:25 +0000

    Implement JBW as gymnasium environment

--- src/gym_continual_rl/__init__.py
+++ src/gym_continual_rl/__init__.py
@@ -14,3 +14,12 @@ register(
     id="gym_continual_rl/PuddleWorld-v0",
     entry_point="gym_continual_rl.envs:PuddleEnv",
 )
+
+register(
+    id="gym_continual_rl/JBW-v0",
+    entry_point="gym_continual_rl.envs:JBWEnv",
+    kwargs={
+        "f_type": "obj",
+        "render": False,
+    },
+)
--- src/gym_continual_rl/envs/__init__.py
+++ src/gym_continual_rl/envs/__init__.py
@@ -1,3 +1,4 @@
 from gym_continual_rl.envs.grid_world import GridWorldEnv  # noqa: F401
+from gym_continual_rl.envs.jbw_env import JBWEnv  # noqa: F401
 from gym_continual_rl.envs.l_mini_grid_env import LMiniGridEnv  # noqa: F401
 from gym_continual_rl.envs.puddle_env import PuddleEnv  # noqa: F401
